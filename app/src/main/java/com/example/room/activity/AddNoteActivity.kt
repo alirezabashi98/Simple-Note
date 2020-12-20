cc39bcff650eab2e47df2becec74aced61ef3a4c
alirezabashi98 <alirezabashi98@gmail.com>
2020-12-20 20:28:55 +0330

    make AddNoteActivity

---
+++ app/src/main/java/com/example/room/activity/AddNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.room.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.room.R
+
+class AddNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
